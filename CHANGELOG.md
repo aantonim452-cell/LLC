ff98008cf4e8b0f5428448293621500b9fc9f963
aantonim452-cell <aantonim452@gmail.com>
2026-04-23 21:06:32 +0800

    Add files via upload

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Changelog LLC Store
+
+## [2.0.0] - 2026-04-23
+### ✨ Added
+- Menu MULTI LAYANAN (MOD, TopUp, Jasa APK, Script, Website, Lainnya)
+- 30+ Game MOD Premium
+- 8+ Item TopUp
+- Form konsultasi jasa langsung ke WA
+- PWA Support (bisa install ke HP & offline)
+
+## [1.0.0] - 2026-03-01
+### 🎉 Initial Release
+- Aplikasi pertama LLC Store
+- Fitur jual koin Higgs, Bos Domino, Royal Domino, Berfist
---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'llcstore-v1';
+const urlsToCache = [
+  '/LLC/',
+  '/LLC/index.html',
+  '/LLC/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,300;400;500;600;700;800;900&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+    })
+  );
+});
